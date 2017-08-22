edb8ad18de372ba9b317b49840aafda4d0f63832
scotty-b <businge.scott@andela.com>
2017-08-22 22:11:27 +0300

    routing file

---
+++ terraform/routing.tf
@@ -0,0 +1,33 @@
+resource "google_compute_firewall" "vof-internal-firewall" {
+  name = "${var.env_name}-vof-internal-network"
+  network = "${google_compute_network.vof-network.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports = ["0-65535"]
+  }
+
+  source_ranges = ["{var.ip_cidr_range}"]
+}
+
+resource "google_compute_firewall" "vof-public-firewall" {
+  name = "${var.env_name}-vof-public-firewall"
+  network = "{google_compute_network.vof-network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  # TODO: Add target_tags for load balancer
+}
