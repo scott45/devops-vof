b016aa0233c721a216a75fac757b31e267d11b70
scotty-b <businge.scott@andela.com>
2017-08-24 10:08:18 +0300

    update file

--- terraform/routing.tf
+++ terraform/routing.tf
@@ -1,5 +1,5 @@
 resource "google_compute_firewall" "vof-internal-firewall" {
-  name = "${var.env_name}-vof-internal-network"
+  name    = "${var.env_name}-vof-internal-network"
   network = "${google_compute_network.vof-network.name}"
 
   allow {
@@ -8,26 +8,27 @@ resource "google_compute_firewall" "vof-internal-firewall" {
 
   allow {
     protocol = "tcp"
-    ports = ["0-65535"]
+    ports    = ["0-65535"]
   }
 
   allow {
     protocol = "udp"
-    ports = ["0-65535"]
+    ports    = ["0-65535"]
   }
 
   source_ranges = ["{var.ip_cidr_range}"]
 }
 
 resource "google_compute_firewall" "vof-public-firewall" {
-  name = "${var.env_name}-vof-public-firewall"
+  name    = "${var.env_name}-vof-public-firewall"
   network = "{google_compute_network.vof-network.name}"
 
   allow {
     protocol = "tcp"
-    ports = ["80", "443"]
+    ports    = ["80", "443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
+
   # TODO: Add target_tags for load balancer
 }
