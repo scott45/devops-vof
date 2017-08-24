1de7f174229a5899804e301f23efbb8bb8f811a9
scotty-b <businge.scott@andela.com>
2017-08-24 10:07:49 +0300

    update file

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,9 +3,9 @@ resource "google_compute_network" "vof-network" {
 }
 
 resource "google_compute_subnetwork" "vof-private-subnetwork" {
-  name = "${var.env_name}-vof-private-subnetwork"
-  region = "${var.region}"
-  network = "${google_compute_network.vof-network.self_link}"
+  name          = "${var.env_name}-vof-private-subnetwork"
+  region        = "${var.region}"
+  network       = "${google_compute_network.vof-network.self_link}"
   ip_cidr_range = "${var.ip_cidr_range}"
 }
 
