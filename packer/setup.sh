22d0565f091f8f431e67a67bbd5a72bb08906a4b
scotty-b <businge.scott@andela.com>
2017-10-04 18:20:25 +0300

    make image with vof code

--- packer/setup.sh
+++ packer/setup.sh
@@ -2,59 +2,47 @@
 
 set -ex
 
-install dependencies() {
+install_dependencies() {
+  sudo apt-get update
 
-    sudo apt-get update
-
-    sudo apt-get install -y --no-install-recommends git-core curl zlib1g-dev \
-        build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev  \
-        sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev wget nodejs      \
-        python-software-properties libffi-dev postgresql postgresql-contrib    \
-        libpq-dev
-
+  sudo apt-get install -y --no-install-recommends git-core curl zlib1g-dev \
+    build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev  \
+    sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev wget nodejs      \
+    python-software-properties libffi-dev postgresql postgresql-contrib    \
+    libpq-dev
 }
 
 install_ruby() {
-  # Ensure the vof user has write & execute  access to /usr/local
+  sudo chgrp -R vof  /usr/local
+  sudo chmod -R g+rw /usr/local
 
-  sudo chown -R vof /usr/local
+  curl -O -L https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.1.tar.gz
+  tar zxf ruby-*
 
-  mkdir /tmp/workspace
-  pushd /tmp/workspace
-    curl -L -O  https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.1.tar.gz
-    tar zxf ruby-2.4.1.tar.gz
-
-    pushd ruby-2.4.1
-      ./configure
-      make && make install
-    popd
+  pushd ruby-2.4.1
+    ./configure
+    make && make install
   popd
-
-  rm -rf /tmp/workspace
-
 }
 
 install_ruby_dependencies() {
+  curl -O -L -k https://rubygems.org/rubygems/rubygems-2.6.12.tgz
+  curl -O -L -k https://rubygems.org/downloads/bundler-1.15.3.gem
 
-    curl -L -O https://rubygems.org/downloads/bundler-1.15.4.gem
-  curl -L -O https://rubygems.org/rubygems/rubygems-2.6.12.tgz
-
-  tar xzf rubygems-2.6.12.tgz
+  tar zxf rubygems-2.6.12.tgz
   pushd rubygems-2.6.12
     ruby setup.rb
   popd
-  gem install bundler-1.15.4.gem --local --no-ri --no-rdoc
 
+  gem install bundler-1.15.3.gem --local --no-ri --no-rdoc
 }
 
 main() {
+  install_dependencies
 
-    install_dependencies
+  install_ruby
 
-    install_ruby
-
-    install_ruby_dependencies
-
+  install_ruby_dependencies
 }
 
 main "$@"
